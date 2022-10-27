2e06ac5e82697ebdfb48459f90c3b62d746d62e2
Abdullah <101mdabdullah@gmail.com>
2022-10-28 00:24:23 +0530

    commit home page update_01

---
+++ css and js/script.js
@@ -0,0 +1,26 @@
+var d=new Date();
+var n=d.getDay();
+            
+switch(n)
+{
+    case 0:
+        document.getElementById("fact").innerHTML="People who love to eat are always the best people!!";
+        break;
+    case 1:
+        document.getElementById("fact").innerHTML="Apples give you more energy than coffee!!";
+        break;
+    case 2:
+        document.getElementById("fact").innerHTML=" Bananas are berries... and strawberries aren't!!"
+        break;
+    case 3:
+        document.getElementById("fact").innerHTML="Almonds Are Not Nuts!!"
+        break;
+    case 4:
+        document.getElementById("fact").innerHTML="Spicy Food Reactions Are Mental, Not Physical!!"
+        break;
+    case 5:
+        document.getElementById("fact").innerHTML="One Cup of Kale Has More Vitamin C Than an Orange!!"
+        break;
+    case 6:
+        document.getElementById("fact").innerHTML="Chocolate was once used as currency!!The Maya civilization used it as money!!"
+}
