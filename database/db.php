858a437338eb9ccb2c16f83c3638f0eb4988bea5
Kainat Hussain <mkhsamsungt2@gmail.com>
2022-10-31 00:33:18 +0530

    datatabase connection , login

---
+++ database/db.php
@@ -0,0 +1,9 @@
+<?php 
+$con = mysqli_connect("sql12.freesqldatabase.com","sql12530306","NcCwFRsvdq","sql12530306");
+
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to Database: " . mysqli_connect_error();
+  }
+  
+?>
---
+++ login.php
@@ -0,0 +1,25 @@
+<?php 
+		session_start();
+		include("database/db.php");
+		// if(isset($_POST['Login'])){
+		// }
+
+        $c_mail = $_POST['email'];
+			$c_pass = $_POST['pass'];
+			
+			$sel_c = "SELECT * from Customer where customer_password='$c_pass' AND customer_email='$c_mail'";
+			
+			$run_c = mysqli_query($con, $sel_c);
+			
+			$check_customer = mysqli_num_rows($run_c); 
+
+            //echo $check_customer;
+			if($check_customer>0 ){
+			$_SESSION['customer_email']=$c_mail; 
+			header("location: Home.html");
+			}	
+            else{
+                echo "<script>alert('Email or password is incorrect, please try again!')</script>";
+                // header("location: Login.html");
+            }
+?>
